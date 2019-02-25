eded2372321abd10dff403b8bb59add3dbf16ce8
Alex Liu <alex_liu@epam.com>
2019-02-25 16:15:34 +0800

    add write page

--- src/pages/write/write.js
+++ src/pages/write/write.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './write.scss';
 import Navbar from '../../components/Navbar/navbar';
+import ReactMarkdown from 'react-markdown';
 
-const Write = () => (
-  <div>
-    <Navbar />
-    posts
-  </div>
-)
+class Write extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '# This is a header\n\nAnd this is a paragraph'
+    }
+  }
+
+  changeInput(e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  savePost() {
+    
+  }
+
+  render() {
+    const { input } = this.state;
+
+    return (
+      <div>
+        <Navbar />
+        <div className="write-container">
+          <div className="write">
+            <textarea onChange={this.changeInput.bind(this)} value={input} />
+          </div>
+          <div className="preview">
+            <ReactMarkdown source={input} escapeHtml={false}/>
+          </div>
+        </div>
+        <button onClick={this.savePost}>保存</button>
+      </div>
+    );
+  }
+}
+
 
 export default Write;
